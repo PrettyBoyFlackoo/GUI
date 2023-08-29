7e8617b8bca220538fed49ecd6f715a0e4a64ed2
PrettyBoyFlackoo <95177386+PrettyBoyFlackoo@users.noreply.github.com>
2023-08-29 18:09:01 +0200

    Delete gui directory

--- gui/elements/elements/data/Data.hx
+++
@@ -1,9 +0,0 @@
-package gui.elements.data;
-
-import h2d.Tile;
-
-typedef Data = {
-    var label:String;
-    @:optional var tile:Tile;
-    @:optional var color:Int;
-}
--- gui/elements/elements/data/Item.hx
+++
@@ -1,9 +0,0 @@
-package gui.elements.data;
-
-typedef Item = {
-    @:optional var x:Float;
-    @:optional var  y:Float;
-    @:optional var width:Float;
-    @:optional var height:Float;
-    @:optional var value:String;
-}
