00897f8bdd0e1a2f0025deb04cd917049baf770d
PrettyBoyFlackoo <95177386+PrettyBoyFlackoo@users.noreply.github.com>
2023-09-14 12:38:57 +0200

    Add files via upload

--- gui/elements/Text.hx
+++ gui/elements/Text.hx
@@ -1,31 +1,37 @@
 package gui.elements;
 
 
+import sys.FileSystem;
+import hxd.fs.LocalFileSystem;
+import hxd.res.Loader;
 import h2d.Font;
-import hxd.Res;
 import h2d.Object;
 import hxd.res.DefaultFont;
 
 class Text extends h2d.Text {
 
+    var loader:Loader;
+
     private function new(x:Float, y:Float, value:String, parent:Object) {
 
         var font:Font = null;
-        
-        try  {
-            var loadedFont = Res.loader.load('GuiFont.fnt');
-            
-            if (loadedFont != null) {
-                font = loadedFont.to(hxd.res.BitmapFont).toFont();
-            } else {
-                trace('not found font');
-            }
-        } catch(e) {
-            trace(e);
 
+
+        ///Load Custom Font
+        loader = new Loader(new LocalFileSystem('src/gui/assets', ''));
+
+        
+        ///Check Custom Font
+        if (FileSystem.exists('src/gui/assets/Font.fnt')) {
+            var customFont = loader.load('Font.fnt').to(hxd.res.BitmapFont).toFont();
+            
+            font = customFont;
+        }
+        else {
             font = DefaultFont.get();
         }
 
+        
         super(font, parent);
 
 
